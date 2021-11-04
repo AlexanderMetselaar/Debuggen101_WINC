1a45d104ae870229693a5ae449ba0c72816bd6c2
Alexander Metselaar <alexander.metselaar@gmail.com>
2021-11-04 11:51:41 +0100

    Functies aangepast

---
+++ Basic_Arrow_functions/script.js
@@ -0,0 +1,34 @@
+// zet de functie om naar een arrow functie
+
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions()
+
+// Bouw bovenstaande functie om naar een arrow-functie
+
+const ikRockArrowfunctions2 = () =>
+    console.log("Joe, ik rock de arrow functions!");
+
+const fivePlusSeven = function () {
+    return 5 + 7
+};
+
+console.log(fivePlusSeven());
+
+const fivePlusSeven2 = () => 5 + 7
+
+console.log(fivePlusSeven2())
+
+let myFunction = (a, b) => a + b
+console.log(myFunction(8, 9))
+
+let addFive = (a) => a + 5
+console.log(addFive(8))
+
+const createObject = () => ({ greeeting: "hoi"});
+
+
+
+
